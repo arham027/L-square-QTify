4c157b723399c6ebc3b2444caf1d1662a2809883
Mohammed Arham <mohammed.arham273@gmail.com>
2023-11-25 22:49:54 +0530

    Test commit

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Styles from "../Navbar/Navbar.module.css";
+import Button from "../Button/Button";
+import Logo from "../Logo/logo";
+import Search from "../Search/Search";
+
+function Navbar(){
+    return(
+        <nav className={Styles.Navbar}>
+            <a href="/">
+                <Logo/>
+            </a>
+            <Search placeholder="search a song of your choise"/>
+            <Button>Give feedback</Button>
+        </nav>
+    )
+}
+export default Navbar;
